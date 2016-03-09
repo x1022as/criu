a9143513737dccd9bc214f542a574735de23ff3a
Pavel Emelyanov <xemul@virtuozzo.com>
2016-03-09 13:51:25 +0300

    unshare: Support pidns
    This one is tricky. When restoring into new pidns we have to provide
    some init for it, and root task cannot be such, as it already has non
    1 pid.
    
    So for pidns unshare insert a fake entry as root with pid 1 and teach
    it to be pseudo-init, i.e. just pick up all died tasks.
    
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>

--- criu/cr-restore.c
+++ criu/cr-restore.c
@@ -1057,6 +1057,8 @@ static int restore_one_task(int pid, CoreEntry *core)
 		ret = restore_one_zombie(core);
 	else if (current->state == TASK_HELPER) {
 		restore_finish_stage(CR_STATE_RESTORE);
+		if (rsti(current)->helper_cb)
+			rsti(current)->helper_cb();
 		ret = 0;
 	} else {
 		pr_err("Unknown state in code %d\n", (int)core->tc->task_state);
@@ -1161,7 +1163,10 @@ static inline int fork_with_pid(struct pstree_item *item)
 		 * Helper entry will not get moved around and thus
 		 * will live in the parent's cgset.
 		 */
-		rsti(item)->cg_set = rsti(item->parent)->cg_set;
+		if (item->parent)
+			rsti(item)->cg_set = rsti(item->parent)->cg_set;
+		else
+			rsti(item)->cg_set = root_cg_set;
 		ca.core = NULL;
 	}
 
