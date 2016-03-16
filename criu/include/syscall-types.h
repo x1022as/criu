e560c14da730141dc45913c2807f02792a8e140c
Pavel Emelyanov <xemul@virtuozzo.com>
2016-03-16 15:24:00 +0300

    unshare: Define UNSHARE_MOUNT_PROC constant
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>
    Acked-by: Andrey Vagin <avagin@virtuozzo.com>

--- criu/include/syscall-types.h
+++ criu/include/syscall-types.h
@@ -71,6 +71,8 @@ struct itimerspec;
 
 #define CLONE_ALLNS	(CLONE_NEWPID | CLONE_NEWNET | CLONE_NEWIPC | CLONE_NEWUTS | CLONE_NEWNS | CLONE_NEWUSER | CLONE_NEWCGROUP)
 
+#define UNSHARE_MOUNT_PROC	0x1
+
 struct rlimit;
 struct rlimit64;
 
