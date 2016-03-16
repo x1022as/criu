1476a6c5d12526d91e63dfe3e06863e01f0c045f
Dmitry Safonov <dsafonov@virtuozzo.com>
2016-03-16 21:46:00 +0300

    pie/restorer: remove unused parameter frm restore_thread_common
    Since commit 22396f769061 ("restore: construct sigframe in crtools")
    sigframe is unused in restore_thread_common, as restore gpregs/fpu regs
    now does construct_sigframe.
    
    Impact: cleanup
    
    Signed-off-by: Dmitry Safonov <dsafonov@virtuozzo.com>
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>

--- criu/pie/restorer.c
+++ criu/pie/restorer.c
@@ -395,8 +395,7 @@ die:
 	return -1;
 }
 
-static int restore_thread_common(struct rt_sigframe *sigframe,
-		struct thread_restore_args *args)
+static int restore_thread_common(struct thread_restore_args *args)
 {
 	sys_set_tid_address((int *)decode_pointer(args->clear_tid_addr));
 
@@ -453,7 +452,7 @@ long __export_restore_thread(struct thread_restore_args *args)
 
 	rt_sigframe = (void *)args->mem_zone.rt_sigframe;
 
-	if (restore_thread_common(rt_sigframe, args))
+	if (restore_thread_common(args))
 		goto core_restore_end;
 
 	ret = restore_creds(args->creds_args, args->ta->proc_fd);
@@ -1209,7 +1208,7 @@ long __export_restore_task(struct task_restore_args *args)
 	 */
 	rt_sigframe = (void *)args->t->mem_zone.rt_sigframe;
 
-	if (restore_thread_common(rt_sigframe, args->t))
+	if (restore_thread_common(args->t))
 		goto core_restore_end;
 
 	/*
