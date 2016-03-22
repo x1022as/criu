x1022as/criu

commit f8a1a1d8d95e9494825a79205af460e36fd8010c

parasite-syscall: get rid of code_syscall{,size} globals
I think, we may safely remove this exports, as:
- code_syscall is used only in one function, which now will have
  it as a parameter;
- code_syscall_size must be equall to BUILTIN_SYSCALL_SIZE under
  BUILD_BUG_ON, so BUILTIN_SYSCALL_SIZE may be used instead.
  (see ptrace(2) - align restriction to PTRACE_PEEKUSER).

This will reduce arch-depended code coupling a little.

(and I also interested in this because of I need __parasite_execute_syscall
to execute 32-bit syscall code on x86_64, which I prefer to leave without
some CONFIG_X86_64 as it's generic code)

Cc: Cyrill Gorcunov <gorcunov@openvz.org>
Signed-off-by: Dmitry Safonov <dsafonov@virtuozzo.com>
Reviewed-by: Cyrill Gorcunov <gorcunov@openvz.org>
Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>

criu/arch/ppc64/crtools.c
@@ -32,8 +32,6 @@ const u32 code_syscall[] = {
 	0x0fe00000		/* twi 31,0,0	*/
 };
 
-const int code_syscall_size = sizeof(code_syscall);
-
 static inline void __check_code_syscall(void)
 {
 	BUILD_BUG_ON(sizeof(code_syscall) != BUILTIN_SYSCALL_SIZE);
@@ -80,7 +78,7 @@ int syscall_seized(struct parasite_ctl *ctl, int nr, unsigned long *ret,
 	regs.gpr[7] = arg5;
 	regs.gpr[8] = arg6;
 
-	err = __parasite_execute_syscall(ctl, &regs);
+	err = __parasite_execute_syscall(ctl, &regs, code_syscall);
 
 	*ret = regs.gpr[3];
 	return err;

criu/arch/aarch64/crtools.c
@@ -27,11 +27,12 @@ const char code_syscall[] = {
 	0x00, 0x00, 0x20, 0xd4		/* BRK #0 */
 };
 
-const int code_syscall_size = round_up(sizeof(code_syscall), sizeof(long));
+static const int
+code_syscall_aligned = round_up(sizeof(code_syscall), sizeof(long));
 
 static inline void __always_unused __check_code_syscall(void)
 {
-	BUILD_BUG_ON(sizeof(code_syscall) != BUILTIN_SYSCALL_SIZE);
+	BUILD_BUG_ON(code_syscall_aligned != BUILTIN_SYSCALL_SIZE);
 	BUILD_BUG_ON(!is_log2(sizeof(code_syscall)));
 }
 
@@ -71,7 +72,7 @@ int syscall_seized(struct parasite_ctl *ctl, int nr, unsigned long *ret,
 	regs.regs[6] = 0;
 	regs.regs[7] = 0;
 
-	err = __parasite_execute_syscall(ctl, &regs);
+	err = __parasite_execute_syscall(ctl, &regs, code_syscall);
 
 	*ret = regs.regs[0];
 	return err;

criu/arch/arm/crtools.c
@@ -28,11 +28,12 @@ const char code_syscall[] = {
 	0xf0, 0x01, 0xf0, 0xe7          /* UDF #32 */
 };
 
-const int code_syscall_size = round_up(sizeof(code_syscall), sizeof(long));
+static const int
+code_syscall_aligned = round_up(sizeof(code_syscall), sizeof(long));
 
 static inline __always_unused void __check_code_syscall(void)
 {
-	BUILD_BUG_ON(sizeof(code_syscall) != BUILTIN_SYSCALL_SIZE);
+	BUILD_BUG_ON(code_syscall_aligned != BUILTIN_SYSCALL_SIZE);
 	BUILD_BUG_ON(!is_log2(sizeof(code_syscall)));
 }
 
@@ -73,7 +74,7 @@ int syscall_seized(struct parasite_ctl *ctl, int nr, unsigned long *ret,
 	regs.ARM_r4 = arg5;
 	regs.ARM_r5 = arg6;
 
-	err = __parasite_execute_syscall(ctl, &regs);
+	err = __parasite_execute_syscall(ctl, &regs, code_syscall);
 
 	*ret = regs.ARM_r0;
 	return err;

criu/arch/x86/crtools.c
@@ -31,11 +31,12 @@ const char code_syscall[] = {
 	0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc	/* int 3, ... */
 };
 
-const int code_syscall_size = round_up(sizeof(code_syscall), sizeof(long));
+static const int
+code_syscall_aligned = round_up(sizeof(code_syscall), sizeof(long));
 
 static inline __always_unused void __check_code_syscall(void)
 {
-	BUILD_BUG_ON(sizeof(code_syscall) != BUILTIN_SYSCALL_SIZE);
+	BUILD_BUG_ON(code_syscall_aligned != BUILTIN_SYSCALL_SIZE);
 	BUILD_BUG_ON(!is_log2(sizeof(code_syscall)));
 }
 
@@ -103,7 +104,7 @@ int syscall_seized(struct parasite_ctl *ctl, int nr, unsigned long *ret,
 	regs.r8  = arg5;
 	regs.r9  = arg6;
 
-	err = __parasite_execute_syscall(ctl, &regs);
+	err = __parasite_execute_syscall(ctl, &regs, code_syscall);
 
 	*ret = regs.ax;
 	return err;

criu/arch/aarch64/include/asm/parasite-syscall.h
@@ -6,9 +6,6 @@ struct parasite_ctl;
 #define ARCH_SI_TRAP TRAP_BRKPT
 
 
-extern const char code_syscall[];
-extern const int code_syscall_size;
-
 void parasite_setup_regs(unsigned long new_ip, void *stack, user_regs_struct_t *regs);
 
 void *mmap_seized(struct parasite_ctl *ctl,

criu/arch/arm/include/asm/parasite-syscall.h
@@ -5,10 +5,6 @@
 #define ARCH_SI_TRAP TRAP_BRKPT
 
 
-extern const char code_syscall[];
-extern const int code_syscall_size;
-
-
 void parasite_setup_regs(unsigned long new_ip, void *stack, user_regs_struct_t *regs);
 
 void *mmap_seized(struct parasite_ctl *ctl,

criu/arch/ppc64/include/asm/parasite-syscall.h
@@ -5,9 +5,6 @@ struct parasite_ctl;
 
 #define ARCH_SI_TRAP TRAP_BRKPT
 
-extern const char code_syscall[];
-extern const int code_syscall_size;
-
 void parasite_setup_regs(unsigned long new_ip, void *stack, user_regs_struct_t *regs);
 
 void *mmap_seized(struct parasite_ctl *ctl,

criu/arch/x86/include/asm/parasite-syscall.h
@@ -8,9 +8,6 @@ struct parasite_ctl;
 #define ARCH_SI_TRAP SI_KERNEL
 
 
-extern const char code_syscall[];
-extern const int code_syscall_size;
-
 void parasite_setup_regs(unsigned long new_ip, void *stack, user_regs_struct_t *regs);
 
 void *mmap_seized(struct parasite_ctl *ctl,

commit 8535b23550fa290e16dbd1eab250aeb7ec2dff41

ppc64: Fix build regressions
The build on powerPC has been broken when doing some code cleanup
recently.

This patch fixes these regressions.

Fixes: f8a1a1d8d95e ("parasite-syscall: get rid of code_syscall{,size} globals")
Fixes: 69d88241584e ("sigframe: move setup_sas & make it inliner")
Signed-off-by: Laurent Dufour <ldufour@linux.vnet.ibm.com>
Acked-by: Dmitry Safonov <dsafonov@virtuozzo.com>
Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>

criu/arch/ppc64/crtools.c
@@ -78,7 +78,7 @@ int syscall_seized(struct parasite_ctl *ctl, int nr, unsigned long *ret,
 	regs.gpr[7] = arg5;
 	regs.gpr[8] = arg6;
 
-	err = __parasite_execute_syscall(ctl, &regs, code_syscall);
+	err = __parasite_execute_syscall(ctl, &regs, (char*)code_syscall);
 
 	*ret = regs.gpr[3];
 	return err;

criu/arch/ppc64/include/asm/restorer.h
@@ -101,7 +101,7 @@ struct rt_sigframe {
 		  "r"(&thread_args[i])		/* %6 */		\
 		: "memory","0","3","4","5","6","7","14","15")
 
-#define RT_SIGFRAME_UC(rt_sigframe) (&rt_sigframe->uc)
+#define RT_SIGFRAME_UC(rt_sigframe) (&(rt_sigframe)->uc)
 #define RT_SIGFRAME_REGIP(rt_sigframe) ((long unsigned int)(rt_sigframe)->uc.uc_mcontext.gp_regs[PT_NIP])
 #define RT_SIGFRAME_HAS_FPU(rt_sigframe) (1)
 #define RT_SIGFRAME_FPU(rt_sigframe) ((rt_sigframe)->uc.uc_mcontext)

criu/sigframe.c
@@ -9,11 +9,12 @@
 static inline void setup_sas(struct rt_sigframe* sigframe, ThreadSasEntry *sas)
 {
 	if (sas) {
-		struct rt_ucontext *uc	= RT_SIGFRAME_UC(sigframe);
+#define UC	RT_SIGFRAME_UC(sigframe)
 
-		uc->uc_stack.ss_sp	= (void *)decode_pointer((sas)->ss_sp);
-		uc->uc_stack.ss_flags	= (int)(sas)->ss_flags;
-		uc->uc_stack.ss_size	= (size_t)(sas)->ss_size;
+		UC->uc_stack.ss_sp	= (void *)decode_pointer((sas)->ss_sp);
+		UC->uc_stack.ss_flags	= (int)(sas)->ss_flags;
+		UC->uc_stack.ss_size	= (size_t)(sas)->ss_size;
+#undef UC
 	}
 }
 
@@ -21,7 +22,7 @@ int construct_sigframe(struct rt_sigframe *sigframe,
 				     struct rt_sigframe *rsigframe,
 				     CoreEntry *core)
 {
-	k_rtsigset_t *blk_sigset = &RT_SIGFRAME_UC(sigframe)->uc_sigmask;
+	k_rtsigset_t *blk_sigset = (k_rtsigset_t*)&RT_SIGFRAME_UC(sigframe)->uc_sigmask;
 
 	if (core->tc)
 		memcpy(blk_sigset, &core->tc->blk_sigset, sizeof(k_rtsigset_t));